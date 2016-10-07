4fa557b37142eacdabb03745883cce078d73fa77
mbonjour <mic.bonjour@hotmail.fr>
2016-10-07 11:51:21 +0200

    nettoyage

--- web/footer.php
+++ web/footer.php
@@ -4,7 +4,6 @@
 			
 				<TR> 
 					<TH><a class="linkNav" href="mailto:info@officetourisme.ch?subject=Demande de renseignements">Contact</a></TH> 
-					<TH><a class="linkNav" href="impressum.php" >Impressum</a></TH> <!-- PHP ICI POUR AFFICHER FICHIER TEXTE DANS MAIN -->
 					<TH><a class="linkNav" href="logout.php" >Logout</a></TH>
 				</TR> 
 			</TABLE>
