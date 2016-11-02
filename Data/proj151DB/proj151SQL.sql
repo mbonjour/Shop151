4f698689741686a2e3860e66a511729155c0f780
mbonjour <mic.bonjour@hotmail.fr>
2016-11-02 21:09:42 +0100

    Delete commande factice au logout si elle contient rien

--- Data/proj151DB/proj151SQL.sql
+++ Data/proj151DB/proj151SQL.sql
@@ -209,4 +209,5 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-INSERT INTO `t_type_adresse` (`id_Type_Adresse`) VALUES ('Livraison'), ('Facturation')
+INSERT INTO `t_type_adresse` (`id_Type_Adresse`) VALUES ('Livraison'), ('Facturation');
+INSERT INTO `t_etat`(`id_Etat`) VALUES ('Traitement'),('Envoye'),('Livre'),('Paye');
--- web/logout.php
+++ web/logout.php
@@ -1,12 +1,27 @@
 <?php
     session_start();
+
+
+    //Delete de la commande si l'ID n'aparaît pas dans la table T_Content
+    include('connectDbSQL.php');
+    $sqlSelectIdCommand="SELECT `id_Commande` FROM `t_commande` WHERE `session_ID`='".session_id()."'";
+    $resultIdCommand=mysql_query($sqlSelectIdCommand);
+    $IdCommand=mysql_result($resultIdCommand,0);
+    $sqlControlContent="SELECT `id_Content` FROM `t_content` WHERE `FK_Commande`=".$IdCommand['id_Commande'];
+    $resultControl=mysql_query($sqlControlContent);
+    $numRows=mysql_num_rows($resultControl);
+    if ($numRows == 0) {
+        $sqlDeleteCommandClient="DELETE FROM `t_commande_client` WHERE `FK_Commande`=".$IdCommand['id_Commande'];
+        mysql_query($sqlDeleteCommandClient);
+        $sqlDeleteCommand="DELETE FROM `t_commande` WHERE `id_Commande`=".$IdCommand['id_Commande'];
+        mysql_query($sqlDeleteCommand);
+    }
+    mysql_close();
+
+
     $_SESSION=array();//on efface toutes les variables de la session
     session_destroy(); // Puis on détruit la session
     setcookie('UserName');
     setcookie('admin');
-
-    //Delete de la commande si l'ID n'aparaît pas dans la table T_Content
-
-    
     header('location:./home.php');
 ?>
